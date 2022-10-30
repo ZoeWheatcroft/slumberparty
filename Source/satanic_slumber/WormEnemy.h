a90ba568fc45ca677fc6fbdf2ceca64bcb379d2a
ZoeWheatcroft <zoe.wheatcroft@gmail.com>
2022-10-30 11:39:32 -0400

    Created Worm stuff

---
+++ Source/satanic_slumber/WormEnemy.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "WormEnemy.generated.h"
+
+UCLASS()
+class SATANIC_SLUMBER_API AWormEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AWormEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
